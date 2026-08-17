ddf60be69923cf47d483af03c111c1022386580a
shree <shreerajgudade124@gmail.com>
2026-08-17 23:16:55 +0530

    the UI

--- fix_tray.py
+++
@@ -1,3 +0,0 @@
-lines = open('src/system_tray.py', encoding='utf-8').read().splitlines()[:405]
-stop_method = ['    def stop(self):', '        """Stop the system tray"""', '        self.running = False', '        if self.icon:', '            self.icon.stop()']
-open('src/system_tray.py', 'w', encoding='utf-8').write('\n'.join(lines + stop_method) + '\n')
